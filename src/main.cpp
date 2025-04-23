51e7b63120eaec237c894476b976368ee85d4a44
Johnny Cena <iamahuman1395@gmail.com>
2025-04-23 08:52:20 +0300

    u

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,3 @@
-#include "MyLayer.hpp"
+#include <Geode/Geode.hpp>
 
-using namespace cocos2d;
-
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-
-    bool init() {
-        if (!MenuLayer::init()) return false;
-        auto spr = CCSprite::createWithSpriteFrameName("GJ_playBtn2_001.png");
-	spr->setScale(.65f);
-	auto btn = CCMenuItemSpriteExtra::create(
-            spr, this, menu_selector(MyMenuLayer::onMyLayer)
-        );
-
-        auto menu = this->getChildByID("bottom-menu");
-        menu->addChild(btn);
-	menu->updateLayout();
-
-	btn->setID("button"_spr);
-
-        return true;
-    }
-
-    void onMyLayer(CCObject*) {
-        (void)geode::cocos::switchToScene(MyLayer::create());
-    }
-};
+using namespace geode::prelude;
